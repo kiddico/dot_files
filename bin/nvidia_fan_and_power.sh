f74c4cf8f4d29b29e8fd579d7a6efdbd71bb7c08
Nick Chirico <nickchirico07@gmail.com>
2017-02-13 14:29:29 -0500

    all the comments!

--- bin/nvidia_fan_and_power.sh
+++ bin/nvidia_fan_and_power.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1
-nvidia-settings -a [gpu:0]/GPUFanControlState=1
-nvidia-settings -a [fan:0]/GPUCurrentFanSpeed=26
+
+# Set the taret speed, or temperature goal.
+nvidia-settings -a [gpu:0]/GPUPowerMizerMode=2
+
+# Allow or disallow manual fan control
+nvidia-settings -a [gpu:0]/GPUFanControlState=0
+
+# Obv sets fan speed
+#nvidia-settings -a [fan:0]/GPUCurrentFanSpeed=26
